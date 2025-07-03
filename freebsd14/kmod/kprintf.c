70ba075665b64d30f3f8e6eebc4158d255fac089
Daisuke MIYAMOTO <miyamoto@ds.itc.u-tokyo.ac.jp>
2025-07-03 15:10:55 +0900

    Update kprintf.c

--- freebsd14/kmod/kprintf.c
+++ freebsd14/kmod/kprintf.c
@@ -9,17 +9,14 @@
 #include <sys/uio.h>
 #include <sys/unistd.h>
 
-// AUE_NULL が未定義なら自前で定義
 #ifndef AUE_NULL
 #define AUE_NULL 0
 #endif
 
-// syscall 引数構造体
 struct kprintf_args {
     char *buf;
 };
 
-// syscall の本体
 static int
 kprintf_syscall(struct thread *td, void *args)
 {
@@ -35,16 +32,13 @@ kprintf_syscall(struct thread *td, void *args)
     return 0;
 }
 
-// syscall エントリ構造体
 static struct sysent kprintf_sysent = {
     .sy_narg = sizeof(struct kprintf_args) / sizeof(register_t),
     .sy_call = kprintf_syscall
 };
 
-// syscall 番号の保存用
 static int offset = NO_SYSCALL;
 
-// モジュールのロード・アンロード時の処理
 static int
 kprintf_load(struct module *module, int cmd, void *arg)
 {
@@ -61,6 +55,5 @@ kprintf_load(struct module *module, int cmd, void *arg)
     return 0;
 }
 
-// syscall の登録
 SYSCALL_MODULE(kprintf, &offset, &kprintf_sysent, kprintf_load, NULL);
 
